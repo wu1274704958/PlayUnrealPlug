880edce01fce819e0944e514df9ec39f9c25fba9
“wws” <1274704958@qq.com>
2023-06-13 23:23:43 +0800

    Draw foot print shader [------]

---
+++ Source/TestPlug/Private/FootPrintRender/FootPrintRenderShaderModel.cpp
@@ -0,0 +1,12 @@
+﻿#include "FootPrintRenderShaderModel.h"
+
+bool FootPrintRenderShader::ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)
+{
+	return IsFeatureLevelSupported(	Parameters.Platform, ERHIFeatureLevel::SM5);
+}
+
+void FootPrintRenderShader::ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters,
+	FShaderCompilerEnvironment& OutEnvironment)
+{
+	FGlobalShader::ModifyCompilationEnvironment(Parameters, OutEnvironment);
+}
---
+++ Source/TestPlug/Private/FootPrintRender/FootPrintRenderShaderModel.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+class FootPrintRenderShader : public FGlobalShader
+{
+	DECLARE_INLINE_TYPE_LAYOUT(FootPrintRenderShader, NonVirtual);
+public:
+	FootPrintRenderShader() = default;
+
+	explicit FootPrintRenderShader(const ShaderMetaType::CompiledShaderInitializerType& Initializer)
+		: FGlobalShader(Initializer)
+	{
+	}
+
+	static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters);
+	static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters, FShaderCompilerEnvironment& OutEnvironment);
+	
+};
