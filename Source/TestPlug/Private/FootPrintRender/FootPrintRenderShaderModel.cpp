c2cd212beb17a1cb437ba83b141f191adf681c38
wws <1274704958@qq.com>
2023-06-15 20:45:35 +0800

    Draw foot print shader [------]

--- Source/TestPlug/Private/FootPrintRender/FootPrintRenderShaderModel.cpp
+++ Source/TestPlug/Private/FootPrintRender/FootPrintRenderShaderModel.cpp
@@ -1,12 +1,30 @@
 ﻿#include "FootPrintRenderShaderModel.h"
 
-bool FootPrintRenderShader::ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)
+bool CopyTextureShader::ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)
 {
 	return IsFeatureLevelSupported(	Parameters.Platform, ERHIFeatureLevel::SM5);
 }
 
-void FootPrintRenderShader::ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters,
+void CopyTextureShader::ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters,
 	FShaderCompilerEnvironment& OutEnvironment)
 {
 	FGlobalShader::ModifyCompilationEnvironment(Parameters, OutEnvironment);
+}
+
+
+IMPLEMENT_SHADER_TYPE(,CopyTextureShaderVS,TEXT("/Shaders/CopyTexture.usf"),TEXT("MainVS"),SF_Vertex)
+IMPLEMENT_SHADER_TYPE(,CopyTextureShaderPS,TEXT("/Shaders/CopyTexture.usf"),TEXT("MainPS"),SF_Pixel)
+
+void DrawCopyTexture_GameThread(FVector2D Offset,const FTexture* InTexture,FTextureRenderTargetResource* OutTextureRenderTargetResource)
+{
+	FRenderThreadScope RenderScope;
+	RenderScope.EnqueueRenderCommand([Offset,InTexture,OutTextureRenderTargetResource](FRHICommandListImmediate& RHICmdList)
+	{
+		FRHITexture2D *RT = OutTextureRenderTargetResource->GetRenderTargetTexture();
+		RHICmdList.Transition(FRHITransitionInfo(RT,ERHIAccess::SRVMask,ERHIAccess::RTV));
+
+		FRHIRenderPassInfo RenderPassInfo(RT,ERenderTargetActions::DontLoad_Store);
+		RHICmdList.BeginRenderPass(RenderPassInfo,TEXT("CopyTexture"));
+		
+	});
 }
--- Source/TestPlug/Private/FootPrintRender/FootPrintRenderShaderModel.h
+++ Source/TestPlug/Private/FootPrintRender/FootPrintRenderShaderModel.h
@@ -1,17 +1,46 @@
 ﻿#pragma once
 
-class FootPrintRenderShader : public FGlobalShader
-{
-	DECLARE_INLINE_TYPE_LAYOUT(FootPrintRenderShader, NonVirtual);
-public:
-	FootPrintRenderShader() = default;
+#define DECLARE_SHADER_BY_BASE(STAGE,BASE) \
+class BASE##STAGE : public BASE	\
+{		\
+	DECLARE_SHADER_TYPE(BASE##STAGE, Global);	\
+	public:		\
+	BASE##STAGE() = default;	\
+	explicit BASE##STAGE(const ShaderMetaType::CompiledShaderInitializerType& Initializer)	\
+		: BASE(Initializer)	\
+	{	\
+	}	\
+};
 
-	explicit FootPrintRenderShader(const ShaderMetaType::CompiledShaderInitializerType& Initializer)
+class CopyTextureShader : public FGlobalShader
+{
+	DECLARE_INLINE_TYPE_LAYOUT(CopyTextureShader, NonVirtual);
+public:
+	CopyTextureShader() = default;
+
+	explicit CopyTextureShader(const ShaderMetaType::CompiledShaderInitializerType& Initializer)
 		: FGlobalShader(Initializer)
 	{
+		Offset.Bind(Initializer.ParameterMap, TEXT("Offset"));
+		Texture.Bind(Initializer.ParameterMap, TEXT("Texture"));
+		Sampler.Bind(Initializer.ParameterMap, TEXT("Sampler"));
 	}
 
 	static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters);
 	static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters, FShaderCompilerEnvironment& OutEnvironment);
-	
+	template <typename TShaderRHIParamRef>
+	void SetParameters(FRHICommandListImmediate& RHICmdList,const TShaderRHIParamRef ShaderRHI,FVector2D Offset,FTexture* InTexture)
+	{
+		SetShaderValue(RHICmdList, ShaderRHI, Offset);
+		SetTextureParameter(RHICmdList, ShaderRHI, Texture,Sampler,InTexture);
+	}
+
+	LAYOUT_FIELD(FShaderResourceParameter, Texture);
+	LAYOUT_FIELD(FShaderResourceParameter, Sampler);
+	LAYOUT_FIELD(FShaderParameter,Offset);
 };
+
+DECLARE_SHADER_BY_BASE(VS, CopyTextureShader);
+DECLARE_SHADER_BY_BASE(PS,CopyTextureShader);
+
+void DrawCopyTexture_GameThread(FVector2D Offset,const FTexture* InTexture,FTextureRenderTargetResource* OutTextureRenderTargetResource);
